d00632c7cadfd321c18c4dbce6cff39380c32dd5
isibtain <isibtainraza@gmail.com>
2020-10-07 01:09:53 -0500

    added frontend

---
+++ src/main/resources/static/precache-manifest.de8ebb3004c52322b10a5f5ad48d766f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b69d3f97508673dbe403f96e21c402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e96dc82daabce23c19bf",
+    "url": "/static/css/main.5d560caf.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "e96dc82daabce23c19bf",
+    "url": "/static/js/main.2ae46dce.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
